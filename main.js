9b6f69c05e916c23c11032db382eea6908a9c2c6
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-04-22 20:48:48 +0200

    completato esercizio operatori 2

---
+++ main.js
@@ -0,0 +1,5 @@
+let annoCorrente = 2024;
+let annoNascita = 1989;
+let age = annoCorrente - annoNascita;
+let toOneHundred = 100 - age;
+console.log(`Hai ${age} anni e ti mancano ${toOneHundred} anni per compierne 100`);
